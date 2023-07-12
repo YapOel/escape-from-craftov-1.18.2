4a32b5d63bad165442bb2cc7780f98c6df085006
YapOel <sborshekov@gmail.com>
2023-07-12 19:44:29 +0300

    0.2.1 License update

---
+++ src/main/java/com/craftov/item/custom/mk47Item.java
@@ -0,0 +1,9 @@
+package com.craftov.item.custom;
+
+import net.minecraft.item.CrossbowItem;
+
+public class mk47Item extends CrossbowItem {
+    public mk47Item(Settings settings) {
+        super(settings);
+    }
+}
